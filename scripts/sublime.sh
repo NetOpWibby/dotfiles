4b245332f81dd00a800eb6ff92458525228fa11b
netop://ウエハ <paul@webb.page>
2020-03-19 17:36:23 -0700

    SMH, missed a backslash

--- scripts/sublime.sh
+++ scripts/sublime.sh
@@ -20,7 +20,7 @@ echo "Transferring Sublime settings"
 echo "============================="
 
 cp -R $PWD/Package%20Control.sublime-package $HOME/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package
-cp -R $PWD/config/sublime/Packages/User/ $HOME/Library/Application\ Support/Sublime\ Text 3/Packages/User
+cp -R $PWD/config/sublime/Packages/User/ $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 rm -rf $PWD/Package%20Control.sublime-package
 
 echo "======================"
