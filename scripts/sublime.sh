8c40da5c0f09f9f89417cc3fd2ba6173518f9cd4
netop://ウエハ <paul@webb.page>
2020-03-19 17:35:37 -0700

    Fixed Sublime script

--- scripts/sublime.sh
+++ scripts/sublime.sh
@@ -2,19 +2,26 @@
 
 
 
-echo "=========================="
-echo "Installing Package Control"
-echo "=========================="
+echo "==========================="
+echo "Downloading Package Control"
+echo "==========================="
 
-# wget -P ~/Library/Application\ Support/Sublime\ Text 3/Installed\ Packages/ "https://packagecontrol.io/Package%20Control.sublime-package"
 curl -O "https://packagecontrol.io/Package%20Control.sublime-package"
-mv $PWD/Package%20Control.sublime-package ~/Library/Application\ Support/Sublime\ Text 3/Installed\ Packages/Package\ Control.sublime-package
+
+echo "===================="
+echo "Creating directories"
+echo "===================="
+
+mkdir -p $HOME/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
+mkdir -p $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
 echo "============================="
 echo "Transferring Sublime settings"
 echo "============================="
 
-cp -R $PWD/config/sublime/Packages/User/ /Users/netopwibby/Library/Application\ Support/Sublime\ Text 3/Packages/User
+cp -R $PWD/Package%20Control.sublime-package $HOME/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package
+cp -R $PWD/config/sublime/Packages/User/ $HOME/Library/Application\ Support/Sublime\ Text 3/Packages/User
+rm -rf $PWD/Package%20Control.sublime-package
 
 echo "======================"
 echo "Sublime setup complete"
