b79250981c7d4bfd68b45b77cd1a3aa639a2cb06
netop://ウエハ <paul@webb.page>
2020-03-29 13:55:54 -0700

    Minor updates

---
+++ scripts/themes.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+
+
+echo "=============="
+echo "Copying themes"
+echo "=============="
+
+cp -R $PWD/.dotfiles/themes/zsh/dedsec.zsh-theme $HOME/.oh-my-zsh/themes
+
+echo "======================"
+echo "Theme copying complete"
+echo "======================"
